eace56cc8fb846b03fb736d316f177dafe4955d1
binhopersio <binhopersio@gmail.com>
2021-04-20 16:12:24 -0300

    new changes

--- src/functions/test.js
+++
@@ -1,8 +0,0 @@
-const { notifier } = require('../sender');
-
-notifier(
-  'Um cliente acaba de fazer um agendamento!',
-  'confira agora mesmo.',
-  'ef5cxtGViwxgBD2S-ZrIG8:APA91bEpEuSXOHnADmO1WxTVZZUca15iyJ0zEgDtUW3lQZk3pIV5HOFBESDtcDLRPVgpEo44Q5vbyJ_W1ZgYh_1J76auzUzFvlRxf6gFbcFiSMVgkzNQmvtpJY-TVd1FpBRP2XiZ1Rlp'
-  // 'ef5cxtGViwxgBD2S-ZrIG8:APA91bEpEuSXOHnADmO1WxTVZZUca15iyJ0zEgDtUW3lQZk3pIV5HOFBESDtcDLRPVgpEo44Q5vbyJ_W1ZgYh_1J76auzUzFvlRxf6gFbcFiSMVgkzNQmvtpJY-TVd1FpBRP2XiZ1Rlp'
-);
