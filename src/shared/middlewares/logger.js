14304ba4e5406765fa60f9438dc0079192461b19
abnerpersio <binhopersio@gmail.com>
2021-09-25 10:16:21 -0300

    adds logger middleware

---
+++ src/shared/middlewares/logger.js
@@ -0,0 +1,50 @@
+import fetch from 'node-fetch';
+
+export default function LoggerMiddleware(req, res, next) {
+  const oldSend = res.send;
+
+  function serializeAndSendToLogger(payload) {
+    let parsedBody = {};
+
+    try {
+      parsedBody = JSON.parse(payload.body);
+    } catch (err) {
+      parsedBody = payload.body;
+    }
+
+    const parsed = {
+      request: {
+        ip: req.ip,
+        path: req.originalUrl,
+        method: req.method,
+        headers: req.headers,
+        body: req.body,
+      },
+      response: {
+        status: payload.status ?? res.statusCode,
+        body: parsedBody,
+      },
+    };
+
+    fetch(
+      'https://node-spreadsheets.vercel.app/api/sheets',
+      {
+        method: 'POST',
+        headers: {
+          user: process.env.LOGGER_USER,
+          pass: process.env.LOGGER_PASS,
+        },
+        body: JSON.stringify(parsed),
+      },
+    );
+  }
+
+  res.send = (body) => {
+    serializeAndSendToLogger({ res, body });
+
+    res.send = oldSend;
+    return res.send(body);
+  };
+
+  next();
+}
