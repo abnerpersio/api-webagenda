87de022864fdaa04282a83e689e9295ea81481ec
abnerpersio <binhopersio@gmail.com>
2021-10-05 17:55:13 -0300

    changes db to using db_name in enviroment

--- src/setup/db.js
+++ src/setup/db.js
@@ -1,7 +1,9 @@
 import '../bootstrap';
 import mongoose from 'mongoose';
 
-const { DB_URI, DB_USER, DB_PASS } = process.env;
+const {
+  DB_URI, DB_USER, DB_PASS, DB_NAME,
+} = process.env;
 
 mongoose.connect(DB_URI, {
   user: DB_USER,
@@ -12,7 +14,7 @@ mongoose.connect(DB_URI, {
   readPreference: 'primary',
   w: 'majority',
   authSource: 'admin',
-  dbName: 'web_agenda',
+  dbName: DB_NAME,
 })
   .then(() => console.log('Conectado ao MongoDB!'))
   .catch(() => console.log('Erro ao conectar ao MongoDB'));
